90f5af6ab4a16e6f292e81ff1ab450dab8a31bd6
dltsao <dltsao@crimson.ua.edu>
2023-04-13 11:26:14 -0500

    empty commit

--- app/askelon_api.py
+++ app/askelon_api.py
@@ -9,19 +9,19 @@ def welcome_message():
     return {"message": "Welcome to the Ask Elon API! Go to '/docs' to see FastAPI doc page"}
 
 @app.get("/generate_tweet")
-async def generate_tweet_api(subject: str, username: str):
+def generate_tweet_api(subject: str, username: str):
     generated_tweet, raw_tweet_list = generate_tweets(subject, username)
     return {"generated_tweet": generated_tweet,  "subject": subject, "username": username, "tweets": raw_tweet_list}
 
 #Generate tweet from given username for a random subject
 @app.get("/generate_tweet_random_subject")
-async def generate_tweet_random_subject_api(username: str):
+def generate_tweet_random_subject_api(username: str):
     subject = random_subject()
     generated_tweet, raw_tweet_list = generate_tweets(subject, username)
     return {"generated_tweet": generated_tweet,  "subject": subject, "username": username, "tweets": raw_tweet_list}
 
 @app.get("/generate_tweet_random")
-async def generate_tweet_random_everything_api():
+def generate_tweet_random_everything_api():
     subject = random_subject()
     username = random_username()
     generated_tweet, raw_tweet_list = generate_tweets(subject, username)
